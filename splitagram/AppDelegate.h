841469539d85df9a338339de53a9d0b482f4ea46
sb2012 <saswata_basu@yahoo.com>
2014-03-18 17:59:26 -0700

    Initial Commit

---
+++ splitagram/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  splitagram
+//
+//  Created by Saswata Basu on 3/18/14.
+//  Copyright (c) 2014 Saswata Basu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ splitagram/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  splitagram
+//
+//  Created by Saswata Basu on 3/18/14.
+//  Copyright (c) 2014 Saswata Basu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
